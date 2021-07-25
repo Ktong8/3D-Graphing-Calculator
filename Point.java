2001e8497cc01d3ac2bba0d3d77756b19afdc928
Kevin Tong <kevinct@mit.edu>
2021-07-25 15:24:53 -0700

    Added specifications for the Point class

--- Point.java
+++ Point.java
@@ -8,25 +8,56 @@ package threed_calculator;
 /**
  *
  * @author kevin
+ * 
+ * An Immutable Point class representing a point in 3D space in rectangular coordinates
  */
 public class Point {
-    double x, y, z;
+    private final double x, y, z;
     
+    /**
+     * Creates a new Point given the rectangular coordinates
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @param z the z coordinate
+     */
     public Point(double x, double y, double z){
         this.x = x;
         this.y = y;
         this.z = z;
     }
     
-    public Point scalarMultiply(double n){
+    /**
+     * Multiplies each coordinate in the point by the given scalar
+     * @param n The scalar to multiply with
+     * @return A new Point object with the new coordinates
+     */
+    public Point scalarMultiply(double n) {
         return new Point(x*n, y*n, z*n);
     }
     
+    /**
+     * Adds another Point to the current Point
+     * @param other The other Point object to add with
+     * @return A New Point resulting from adding the original two Points
+     */
     public Point add(Point other){
         return new Point(other.x + this.x, other.y+this.y, other.z+this.z);
     }
     
-    public double magnitude(){
+    /**
+     * Gives the magnitude of the current Point
+     * @return A double representing the magnitude of the current Point
+     */
+    public double magnitude() {
         return Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2)+Math.pow(z,2));
+    }
+    
+    /**
+     * Computes the Dot Product of this Point with another Point
+     * @param other the other Point to dot product with
+     * @return the scalar result of the dot product
+     */
+    public double dotProduct(Point other) {
+        return this.x * other.x + this.y * other.y + this.z * other.z;
     }
 }
