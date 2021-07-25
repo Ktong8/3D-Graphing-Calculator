5c580014ac467b78710bfa0d6b39220483a030f0
Kevin Tong <kevinct@mit.edu>
2021-07-25 15:19:48 -0700

    Added specifications for each function in the Matrix3 class

--- Matrix3.java
+++ Matrix3.java
@@ -5,17 +5,32 @@
  */
 package threed_calculator;
 
+import java.awt.Point;
+
 /**
  *
  * @author kevin
+ * 
+ * A 3x3 Matrix class
  */
 public class Matrix3 {
-    public double[] values;
+    // A row-major array of values in the matrix;
+    private final double[] values;
     
+    /**
+     * Create a 3x3 matrix from the given row-major array
+     * @param values
+     */
     Matrix3(double[] values){
+        assert(values.length == 9);
         this.values = values;
     }
     
+    /**
+     * Matrix multiply this Matrix3 with another Matrix3
+     * @param other the other Matrix3 to multiply with
+     * @return The resulting Matrix3 from the matrix multiplication
+     */
     public Matrix3 multiply(Matrix3 other){
         double[] result = new double[9];
         for(int row = 0; row<3; row++){
@@ -28,13 +43,22 @@ public class Matrix3 {
         return new Matrix3(result);
     }
     
+    /**
+     * Use this Matrix3 as a rotation matrix to the given point
+     * @param orig The original point to be rotated
+     * @return The result of rotating orig using this rotation matrix
+     */
     public Point rotate(Point orig){
-         return new Point(
-            orig.x * values[0] + orig.y * values[3] + orig.z * values[6],
-            orig.x * values[1] + orig.y * values[4] + orig.z * values[7],
-            orig.x * values[2] + orig.y * values[5] + orig.z * values[8]);
+        return new Point(
+            orig.x * values[0] + orig.y * values[3 * 1] + orig.z * values[3 * 2],
+            orig.x * values[3 * 0 + 1] + orig.y * values[3 * 1 + 1] + orig.z * values[3 * 2 + 1],
+            orig.x * values[3 * 0 + 2] + orig.y * values[3 * 2 + 2] + orig.z * values[3 * 2 + 2]);
      }
     
+     /**
+      * Gives the cross product determinant
+      * @return A new point representing this determinant
+      */
      public Point crossDeterminant(){
          double x = values[0]*(values[4]*values[8]-values[5]*values[7]);
          double y = values[1]*(values[3]*values[8]-values[5]*values[6]);
